625b1435c08cc4df5cc3fff1c7109c9d0ddfd4b2
jones <jones>
2017-02-21 09:34:28 +0530

    till retrieving data using http

--- app/products/product-filter.pipe.ts
+++ app/products/product-filter.pipe.ts
@@ -3,7 +3,7 @@ import {IProduct} from './product'
 
 @Pipe({
     name: 'productFilter'
-}
+})
 export class ProductFilterPipe implements PipeTransform{
     transform(value:IProduct[],filterBy:string): IProduct[]{
         filterBy=filterBy?filterBy.toLocaleLowerCase():null;
